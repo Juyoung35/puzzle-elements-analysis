5bd0ec29c1a83a5ba1c75eb4115a07407f849fb3
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-11-29 19:18:01 +0900

    Update mod.rs

--- src/components/number/mod.rs
+++ src/components/number/mod.rs
@@ -12,3 +12,16 @@ macro_rules! define_numbers {
 
 define_numbers!(NormalNumber, LargeNumber, MediumNumber, SmallNumber);
 
+pub struct Candidates {
+  candidates: [bool; 9],
+}
+
+pub struct CornerNumber {
+  corner: corner,
+  string: String,
+}
+
+pub struct PaddingNumber {
+  padding: padding,
+  string: String,
+}
