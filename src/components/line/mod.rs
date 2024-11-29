28b93c2e91b78d714ad61c722920125a37ee6b58
Juyoung35 <68434709+Juyoung35@users.noreply.github.com>
2024-11-29 17:51:23 +0900

    Update mod.rs

--- src/components/line/mod.rs
+++ src/components/line/mod.rs
@@ -4,3 +4,16 @@ pub trait Line {
 pub struct NormalLine {
   cells: (cell, cell),
 }
+
+pub struct DiagonalLine {
+  cells: (cell, cell),
+}
+
+pub struct FreeLine {
+  cells: (cell, cell),
+}
+
+pub struct MiddleLine {
+  cell: cell,
+  side: side,
+}
